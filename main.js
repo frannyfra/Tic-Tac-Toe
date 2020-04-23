4507e57da6898bd22c6da73bd1f07e00f8303279
frannyfra <francescadelaurentis1987@gmail.com>
2020-04-23 23:08:24 +0100

    implemented reset function

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-const board = ["", "", "",
-    "", "", "",
-    "", "", ""];
 
 const winningCombos = [
     [0, 1, 2],
@@ -12,44 +9,63 @@ const winningCombos = [
     [0, 4, 8],
     [2, 4, 6]
 ];
-
-
-const squares = Array.from(document.querySelectorAll(".square"))
+const squares = Array.from(document.querySelectorAll("#square"));
+const resetButton = document.getElementById("reset-button");
+const boardContainer = document.getElementById("board");
 console.log(squares)
+let messages = document.querySelector("h2");
+let turn = "X";
+let win;
+let board;
+
+const initialise = () => {
+    squares.forEach(square =>
+        square.innerHTML = "");
+
+    board = ["", "", "",
+        "", "", "",
+        "", "", ""];
+    messages.textContent = "Play again!"
+}
+initialise()
 
 const render = () => {
     board.forEach((mark, index) => {
-        console.log(index)
-        console.log(mark)
-        squares[index].textContent = mark;
-    })
+        if (squares[index].textContent === "") squares[index].textContent = mark;
+
+    });
+    messages.textContent = (win === 'T') ? `That's a tie` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
 }
 render();
 
 
-let turn = "X";
-
-// const getWinner = () => {
-//     winningCombos.forEach((combo, index) => {
-//         console.log(combo, index);
-
-//     })
-
-// }
-// getWinner()
+const getWinner = () => {
+    let winner;
+    winningCombos.forEach((combo, index) => {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {
+            winner = board[combo[0]];
+            setTimeout(function () { alert(`${win} wins the game!`), initialise() }, 400);
+        }
+    })
+    return winner ? winner : board.includes('') ? null : 'T';
+}
 
 const handleTurn = (event) => {
-    let index = squares.findIndex((square) => {
-        return square === event.target;
+    const index = squares.findIndex((square) => {
+        if (square.innerHTML === "") {
+            return square === event.target;
+        }
+        else null;
     })
     board[index] = turn;
-    console.log(board, "iam the board");
-    turn = (turn === "X") ? "O" : "X";
-
-    // win = getWinner();
-
+    turn = turn === "X" ? "O" : "X";
+    win = getWinner();
     render();
 }
-document.getElementById("board").addEventListener("click", handleTurn);
+
+resetButton.addEventListener("click", initialise);
+boardContainer.addEventListener("click", handleTurn);
+
+
 
 
